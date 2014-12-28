6e2d030a83c34154db86914d3f6487598d9bfb41
Jeet Sukumaran <jeetsukumaran@gmail.com>
2014-12-27 20:54:15 -0500

    Abstract out exception classes

---
+++ archipelago/error.py
@@ -0,0 +1,13 @@
+
+
+class ArchipelagoException(Exception):
+    pass
+
+class FailedSimulationException(ArchipelagoException):
+    pass
+
+class InsufficientFocalAreaLineagesSimulationException(FailedSimulationException):
+    pass
+
+class TotalExtinctionException(FailedSimulationException):
+    pass
