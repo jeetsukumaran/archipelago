4e598275ed61db73e99fe590ad53e0d214a223c9
Jeet Sukumaran <jeetsukumaran@gmail.com>
2015-02-12 00:06:35 -0500

    update R script

--- R/analyze-dapc.R
+++ R/analyze-dapc.R
@@ -43,11 +43,11 @@ filter.data <- function(summary.df,
 }
 
 # Given a data.frame, returns a list with two named elements:
-# `groups`: data.frame
-#   A data.frame consisting of a single-column, the groupsing variable
+# `group`: data.frame
+#   A data.frame consisting of a single-column, the grouping variable
 # `predictors`: data.frame
 #   A data.frame consisting of (just) the predictor variables.
-create.groups.and.predictors = function(summary.df,
+create.group.and.predictors = function(summary.df,
                                        filter.for.birth.rate=NULL,
                                        filter.for.dispersal.rate=NULL,
                                        filter.for.trait.transition.rate=NULL) {
@@ -56,10 +56,10 @@ create.groups.and.predictors = function(summary.df,
                             filter.for.dispersal.rate=filter.for.dispersal.rate,
                             filter.for.trait.transition.rate=filter.for.trait.transition.rate)
     source.df = na.omit(source.df)
-    groups = source.df[[GROUPING.FIELD.NAME]]
+    group = source.df[[GROUPING.FIELD.NAME]]
     predictors = source.df[,!(names(source.df) %in% NON.PREDICTOR.FIELD.NAMES)]
     rv = list(
-        groups=groups,
+        group=group,
         predictors=predictors
         )
     rv
@@ -68,25 +68,55 @@ create.groups.and.predictors = function(summary.df,
 # Primary workhorse function.
 # Carries out the DAPC analysis, and packages the results.
 # TODO: generalize from "constrained"/"unconstrained"
-calculate.dapc = function(predictors, groups, n.pca, n.da) {
-    dapc.result = dapc(predictors, groups, n.pca=n.pca, n.da=n.da)
+calculate.dapc = function(predictors, group, n.pca, n.da) {
+    dapc.result = dapc(predictors, group, n.pca=n.pca, n.da=n.da)
     var.contr = data.frame(var=rownames(dapc.result$var.contr),
                         LD1=as.vector(dapc.result$var.contr)
                         )
     var.contr = var.contr[order(-var.contr$LD1),]
-    model.prefs = data.frame(groups=groups, pp.model=dapc.result$posterior)
-    mean.pp.for.correct.model = mean(c(model.prefs[model.prefs$groups == "constrained",]$pp.model.constrained, model.prefs[model.prefs$groups == "unconstrained",]$pp.model.unconstrained))
-    # debug1 = nrow(model.prefs[model.prefs$groups=="constrained" & model.prefs$pp.model.constrained>0.5,])
-    # debug2 = nrow(model.prefs[model.prefs$groups=="unconstrained" & model.prefs$pp.model.unconstrained>0.5,])
-    mean.count.correct.model.preferred = nrow(model.prefs[model.prefs$groups=="constrained" & model.prefs$pp.model.constrained>0.5,]) + nrow(model.prefs[model.prefs$groups=="unconstrained" & model.prefs$pp.model.unconstrained>0.5,])
-    mean.prop.correct.model.preferred = mean.count.correct.model.preferred / nrow(model.prefs)
+    model.prefs = data.frame(
+                             group=group,                        # correct model
+                             assign=dapc.result$assign,          # assigned model
+                             # pp.model=dapc.result$posterior    # expands to `pp.model.{group}` for each value in `group`
+                             data.frame(dapc.result$posterior)   # expands to one column for each model, with posterior probability of assignment to that model as value
+                             )
+
+    misassigns = model.prefs[model.prefs$group != model.prefs$assign,1]
+    correct.assigns = model.prefs[model.prefs$group == model.prefs$assign,1]
+    stopifnot(length(correct.assigns) + length(misassigns) == nrow(model.prefs))
+
+    misassigns.prop = prop.table(table(misassigns))
+    mean.prop.wrong.model.preferred = length(misassigns) / nrow(model.prefs)
+    correct.assigns.prop = prop.table(table(correct.assigns))
+    mean.prop.correct.model.preferred = length(correct.assigns) / nrow(model.prefs)
+
+    pps.of.correct.model = c()
+    for (model.name in unique(model.prefs$group)) {
+        pps.of.correct.model = c(pps.of.correct.model, model.prefs[model.prefs$group == model.name, model.name])
+    }
+
+    mean.pp.of.correct.model = sum(pps.of.correct.model) / length(pps.of.correct.model)
+
+    # mean.pp.of.correct.model = mean(c(model.prefs[model.prefs$group == "constrained",]$pp.model.constrained, model.prefs[model.prefs$group == "unconstrained",]$pp.model.unconstrained))
+    # debug1 = nrow(model.prefs[model.prefs$group=="constrained" & model.prefs$pp.model.constrained>0.5,])
+    # debug2 = nrow(model.prefs[model.prefs$group=="unconstrained" & model.prefs$pp.model.unconstrained>0.5,])
+    # mean.count.correct.model.preferred = nrow(model.prefs[model.prefs$group=="constrained" & model.prefs$pp.model.constrained>0.5,]) + nrow(model.prefs[model.prefs$group=="unconstrained" & model.prefs$pp.model.unconstrained>0.5,])
+    # mean.prop.correct.model.preferred = mean.count.correct.model.preferred / nrow(model.prefs)
+
     rv = list(
               dapc.result=dapc.result,
               var.contr=var.contr,
               model.prefs=model.prefs,
-              mean.pp.for.correct.model=mean.pp.for.correct.model,
-              mean.count.correct.model.preferred=mean.count.correct.model.preferred,
-              mean.prop.correct.model.preferred=mean.prop.correct.model.preferred
+              # mean.pp.of.correct.model=mean.pp.of.correct.model,
+              # mean.count.correct.model.preferred=mean.count.correct.model.preferred,
+              # mean.prop.correct.model.preferred=mean.prop.correct.model.preferred,
+              misassigns=misassigns,
+              misassigns.prop=misassigns.prop,
+              correct.assigns=correct.assigns,
+              mean.prop.wrong.model.preferred=mean.prop.wrong.model.preferred,
+              correct.assigns.prop=correct.assigns.prop,
+              mean.prop.correct.model.preferred=mean.prop.correct.model.preferred,
+              mean.pp.of.correct.model=mean.pp.of.correct.model
               )
     rv
 }
@@ -97,20 +127,20 @@ assess.predictor.performance = function(summary.df,
                                         filter.for.birth.rate=NULL,
                                         filter.for.dispersal.rate=NULL,
                                         filter.for.trait.transition.rate=NULL) {
-    x = create.groups.and.predictors.for.regime(summary.df=summary.df,
+    x = create.group.and.predictors.for.regime(summary.df=summary.df,
                                                 filter.for.birth.rate=filter.for.birth.rate,
                                                 filter.for.dispersal.rate=filter.for.dispersal.rate,
                                                 filter.for.trait.transition.rate=filter.for.trait.transition.rate)
-    groups = x$groups
+    group = x$group
     predictors = x$predictors
     result = data.frame()
     for (n.pca in 2:ncol(predictors)) {
         n.da = 10
         # for (n.da in 1:ncol(predictors)) {
-            x = calculate.dapc(predictors, groups, n.pca, n.da)
+            x = calculate.dapc(predictors, group, n.pca, n.da)
             cat(paste(n.pca,
                         n.da,
-                        x$mean.pp.for.correct.model,
+                        x$mean.pp.of.correct.model,
                         x$mean.prop.correct.model.preferred,
                         "\n",
                         sep="\t\t"
@@ -118,7 +148,7 @@ assess.predictor.performance = function(summary.df,
             subresult = data.frame(
                         n.pca = n.pca,
                         n.da = n.da,
-                        mean.pp.for.correct.model=x$mean.pp.for.correct.model,
+                        mean.pp.of.correct.model=x$mean.pp.of.correct.model,
                         mean.prop.correct.model.preferred=x$mean.prop.correct.model.preferred
                         )
             result = rbind(result, subresult)
@@ -134,15 +164,15 @@ analyze.dapc = function(
                         filter.for.birth.rate=NULL,
                         filter.for.dispersal.rate=NULL,
                         filter.for.trait.transition.rate=NULL) {
-    x = create.groups.and.predictors(summary.df=summary.df,
+    x = create.group.and.predictors(summary.df=summary.df,
                                      filter.for.birth.rate=filter.for.birth.rate,
                                      filter.for.dispersal.rate=filter.for.dispersal.rate,
                                      filter.for.trait.transition.rate=filter.for.trait.transition.rate)
-    groups = x$groups
+    group = x$group
     predictors = x$predictors
     rv = calculate.dapc(
             predictors=predictors,
-            groups=groups,
+            group=group,
             n.pca=n.pca,
             n.da=n.da)
     # rv
@@ -171,7 +201,7 @@ analyze.parameter.space.discrete = function(summary.df) {
                             dispersal.rate=dispersal.rate,
                             trait.transition.rate=trait.transition.rate,
                             i1,
-                            x$mean.pp.for.correct.model,
+                            x$mean.pp.of.correct.model,
                             x$mean.prop.correct.model.preferred,
                             "\n",
                             sep="\t\t"
@@ -181,7 +211,7 @@ analyze.parameter.space.discrete = function(summary.df) {
                                        dispersal.rate=dispersal.rate,
                                        trait.transition.rate=trait.transition.rate,
                                        i1=i1,
-                                       mean.pp.for.correct.model=x$mean.pp.for.correct.model,
+                                       mean.pp.of.correct.model=x$mean.pp.of.correct.model,
                                        mean.prop.correct.model.preferred=x$mean.prop.correct.model.preferred
                                        )
                 result = rbind(result, subresult)
@@ -193,19 +223,14 @@ analyze.parameter.space.discrete = function(summary.df) {
 
 classify.trees = function(path.to.trees.summary, path.to.simulation.summary) {
     summary.df <- read.csv(path.to.simulation.summary, header<-T)
-    training.data = create.groups.and.predictors(summary.df)
+    training.data = create.group.and.predictors(summary.df)
     trained.model = calculate.dapc(
             training.data$predictors,
-            training.data$groups,
+            training.data$group,
             n.pca=ncol(training.data$predictors),
             n.da=10)
     trees.df <- read.csv(path.to.trees.summary, header<-T)
-    to.classify.data = create.groups.and.predictors(trees.df)
+    to.classify.data = create.group.and.predictors(trees.df)
     pred.sup <- predict.dapc(trained.model$dapc.result, newdata=to.classify.data$predictors)
 }
 
-
-run = function() {
-    x1 = read.csv("data.csv", header=T)
-    x2 = analyze.dapc(x1, 10, 10)
-}
